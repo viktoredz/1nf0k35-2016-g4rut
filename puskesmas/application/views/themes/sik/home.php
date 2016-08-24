a689477ab75342648db5a390adce92794b1bdd23
Viktor <xu.redzonez@gmail.com>
2016-08-24 12:37:04 +0700

    no message

--- puskesmas/application/views/themes/sik/home.php
+++ puskesmas/application/views/themes/sik/home.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>{title}</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <link rel="icon" href="<?php echo base_url()?>public/themes/login/img/favicon.ico">
     <style type="text/css">
         @import url(<?php echo base_url()?>public/themes/sik/bootstrap/css/bootstrap.min.css);
         @import url(<?php echo base_url()?>public/themes/sik/bootstrap/css/font-awesome.min.css);
